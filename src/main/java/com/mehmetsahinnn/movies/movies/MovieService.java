09743e66853913d6fe6ec441afb71d69596cc255
Mehmet <mehmetsahinn2002@gmail.com>
2023-01-30 12:40:51 +0300

    First Commit

---
+++ src/main/java/com/mehmetsahinnn/movies/movies/MovieService.java
@@ -0,0 +1,24 @@
+package com.mehmetsahinnn.movies.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository repository;
+
+    public MovieService(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
